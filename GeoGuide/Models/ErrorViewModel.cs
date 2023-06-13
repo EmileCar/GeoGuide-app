7b75dc786f6589589e1c84c466f0ba928973bba9
caron <caron@LAPTOP_CARON>
2023-06-13 21:23:18 +0200

    Add project files.

---
+++ GeoGuide/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeoGuide.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
